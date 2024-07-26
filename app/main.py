f0bb392ff0bb5d2c58b72f53cf93287ce92fd89d
TuongHH <tuongho.dev@gmail.com>
2024-07-26 21:53:31 +0700

    [chore]: update for nginx

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routers import image_evaluate, image_extract_classify
 
-app = FastAPI()
+app = FastAPI(root_path="/api/v1")
 
-app.include_router(image_evaluate.router, prefix="/api/v1")
-app.include_router(image_extract_classify.router, prefix="/api/v1")
+app.include_router(image_evaluate.router)
+app.include_router(image_extract_classify.router)
