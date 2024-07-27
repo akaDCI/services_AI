0cf708e6f27d3dedf5aa8fb4a28e0ab67f23eed1
TuongHH <tuongho.dev@gmail.com>
2024-07-27 08:35:41 +0700

    [code]: allow CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from app.routers import image_evaluate, image_extract_classify
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Response
 
 app = FastAPI(root_path="/api/v1")
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(image_evaluate.router)
 app.include_router(image_extract_classify.router)
